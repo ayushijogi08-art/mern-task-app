d4b4a9d287285454e9eeb50f8301cf7e4812d256
Ayushi <ayushijogi@08gmail.com>
2026-02-15 18:14:33 +0530

    updatet  api

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://mern-task-app-oc7l.onrender.com",
 });
 
 api.interceptors.request.use((config) => {
